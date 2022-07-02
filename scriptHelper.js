2a5c384e220e407e751a3aa311ab99133e94b4cb
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-07-02 17:16:44 -0500

    saturday work

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,34 +1,66 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+let pilotName = document.querySelector("input[name=pilotName]");
+let copilotName = document.querySelector("input[name=copilotName]");
+let fuelLevel = document.querySelector("input[name=fuelLevel]");
+let cargoMass = document.querySelector("input[name=cargoMass]");
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-   
+
+    if (testInput === "" || testInput === null || testInput === 0) {
+        return 'Empty'
+    } else if ((!isNaN(Number(testInput)))) {
+        return 'Is a Number'
+    } else {
+        return 'Not a Number'
+    }
+
 }
+if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {
+    alert('Please enter numerical values for Fuel Level and Cargo Mass');
+} else if (validateInput(pilot) === 'Is a Number' || validateInput(copilot) === 'Is a Number') {
+    alert('Please do not enter numbers for name of pilot or co-pilot');
+}
+// if (testInput === "" || testInput === null || testInput === 0) {
+//     return `Empty`
+// } else if ((!isNaN(Number(testInput)))) {
+//     return `Is a Number`
+// } else {
+//     return 'Not a Number'
+// }
+
+
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    let pilotStatus = document.getElementById('pilotStatus');
+    let copilotStatus = document.getElementById('copilotStatus');
+    let fuelStatus = document.getElementById('fuelStatus');
+    let launchStatus = document.getElementById('launchStatus');
+    let cargoStatus = document.getElementById('cargoStatus');
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -39,5 +71,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
